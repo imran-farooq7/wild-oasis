ecdadf20040c97d167e710cc4701435dc0e9155e
Imran Farooq <imran_farooq7@hotmail.com>
2024-01-06 16:56:07 +0500

    styled component installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+
 const App = () => {
-	return <div>App</div>;
+	const H1 = styled.h1`
+		font-size: 30px;
+		color: blue;
+	`;
+
+	const StyledApp = styled.div`
+		background: orangered;
+		padding: 1.2rem 1.6rem;
+	`;
+	return (
+		<>
+			<GlobalStyles />
+			<StyledApp>
+				<H1>Wild Oasis</H1>
+				<Button>Click Me</Button>
+			</StyledApp>
+		</>
+	);
 };
 export default App;
